a4f48de44293f75ad155c8b15d86a47c12a84b57
jeremy.debras <jeremy.debras@scalian.com>
2022-01-20 11:10:55 +0100

    Add Validators in the fields of the form

--- src/app/action/action.component.ts
+++ src/app/action/action.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-action',
@@ -8,12 +13,26 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class ActionComponent implements OnInit {
   public actionForm: FormGroup = this.formBuilder.group({
-    defaultType: [''],
-    operatorName: [''],
+    defaultType: ['', Validators.required],
+    operatorName: ['', [Validators.required, Validators.pattern('[^0-9]+')]],
     remarks: [''],
   });
 
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {}
+
+  public errorDefaultTypeMessage() {
+    return this.actionForm.controls['defaultType'].invalid
+      ? 'Please choose type of default'
+      : null;
+  }
+
+  public errorOperatorNameMessage() {
+    return this.actionForm.controls['operatorName'].hasError('required')
+      ? 'Please enter operator name'
+      : this.actionForm.controls['operatorName'].hasError('pattern')
+      ? 'The field cannot contain numbers'
+      : null;
+  }
 }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,6 +3,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   imports: [
@@ -10,12 +12,16 @@ import { MatInputModule } from '@angular/material/input';
     MatToolbarModule,
     MatSelectModule,
     MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   exports: [
     MatFormFieldModule,
     MatToolbarModule,
     MatSelectModule,
     MatInputModule,
+    MatButtonModule,
+    MatIconModule,
   ],
 })
 export class MaterialModule {}
